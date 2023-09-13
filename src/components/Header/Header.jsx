9db1bc0bdd08a9dd06d58628f5da1af6f4e899ce
Mateus Schiavi <mtsschiavi@gmail.com>
2023-09-13 13:39:44 -0300

    primer commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h1>Header</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At enim dolor sequi quo vitae ad blanditiis assumenda ipsum nulla velit delectus, iste suscipit aspernatur tempore ullam! Vitae ratione sint alias nulla at quibusdam perspiciatis fugit deleniti magnam hic iste consequatur incidunt asperiores accusantium quasi illo reiciendis magni, vel necessitatibus iusto voluptatum. Eaque necessitatibus enim quibusdam similique non molestias, tempora corrupti tempore ipsa aperiam accusamus? Animi praesentium unde officiis, amet, quasi inventore sed ducimus, cumque voluptatibus odio ipsum error laudantium! Quos sequi minima tempore totam, numquam dolores perspiciatis repellat, distinctio eaque similique necessitatibus non consequuntur ab reiciendis fuga veritatis, suscipit amet.</p>
+    </div>
+  )
+}
+
+export default Header
