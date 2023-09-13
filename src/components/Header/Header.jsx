f39a11d9fe200abcc46bb22361115a0fb39d9ff7
jonatan <framan2403@gmail.com>
2023-09-13 12:57:19 -0500

    encabezado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import logo from '../../../public/logo.svg'
+import './Header.css'
 
 function Header() {
   return (
-    <div>
-        <h1>Header</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At enim dolor sequi quo vitae ad blanditiis assumenda ipsum nulla velit delectus, iste suscipit aspernatur tempore ullam! Vitae ratione sint alias nulla at quibusdam perspiciatis fugit deleniti magnam hic iste consequatur incidunt asperiores accusantium quasi illo reiciendis magni, vel necessitatibus iusto voluptatum. Eaque necessitatibus enim quibusdam similique non molestias, tempora corrupti tempore ipsa aperiam accusamus? Animi praesentium unde officiis, amet, quasi inventore sed ducimus, cumque voluptatibus odio ipsum error laudantium! Quos sequi minima tempore totam, numquam dolores perspiciatis repellat, distinctio eaque similique necessitatibus non consequuntur ab reiciendis fuga veritatis, suscipit amet.</p>
+    <div className= 'titulo'>
+        <div>
+       <ul className= 'lista'>
+          <div>
+            <img src={logo} alt="lgogo de la pagina" />
+            </div>
+          <li>
+             About
+          </li>
+          <li>
+            Services
+          </li>
+          <li>
+            Projects
+          </li>
+          <li>
+            CONTACT
+          </li>
+        </ul>
+          </div>
+        <h1>WE ARE CREATIVES</h1>
+        
     </div>
   )
 }
